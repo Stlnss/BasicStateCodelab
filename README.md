3a69650d8666b4bdb837b07f3b93c443016e965f
stlnss <stlnesslv@gmail.com>
2023-04-27 21:17:42 +1000

    State hoisting 10/13

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # BasicStateCodelab
+
 training for State
+
+# References
+
+- [State in Jetpack Compose](https://developer.android.com/codelabs/jetpack-compose-state?index=..%2F..index)
+- [Compose Architecture](https://developer.android.com/jetpack/compose/architecture#udf-compose)
+
--- app/src/main/java/com/still/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/still/basicstatecodelab/WaterCounter.kt
@@ -7,22 +7,30 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.still.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+fun StateFullCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StateLessCounter(count = count, onIncrement = { count++ }, modifier = modifier)
+}
 
+@Composable
+fun StateLessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
         if (count > 0) {
-            Text(text = "You've had $count glasses.")
+            Text("You've had $count glasses.")
         }
-        Button(onClick = { count++ }, modifier = modifier.padding(8.dp), enabled = count < 10) {
-            Text(text = "Add one")
+        Button(onClick = { onIncrement }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
     }
 }
